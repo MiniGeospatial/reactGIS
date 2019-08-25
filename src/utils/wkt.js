8d6b7ec390db67337c3ca943e81f084b0e589dc2
Mike Harris <mike.harris@groundsure.com>
2019-08-25 01:32:45 +0100

    fixes #6

--- src/utils/wkt.js
+++ src/utils/wkt.js
@@ -1,31 +1,63 @@
 const fromWkt = (wkt) => {
-  const numbs = removeText(wkt);
-  const nodes = extractNumbers(numbs);
-  return nodes;
+  const removedCase = wkt.toUpperCase();
+  const geomType = geometryType(removedCase);
+  const nodeString = removeText(geomType, removedCase);
+  const nodes = extractNumbers(geomType, nodeString);
+  return {geometryType: geomType, nodes: nodes};
 }
 
-const removeText = (wkt) => {
-  const lowercase = wkt
-    .toLowerCase()
-    .replace('polygon', '')
-    .replace('((', '')
-    .replace('))', '')
-    .trim();
-  return lowercase;
+const toWkt = (geomType, nodes) => {
+  if (geomType === 'POINT') {
+    return `POINT(${nodes[0]} ${nodes[1]})`;
+  }
 
+  const nodeString = nodes.map(xy => `${xy[0]} ${xy[1]}`).join(', ');
+
+  if (geomType === 'POLYGON') {
+    return `POLYGON((${nodeString}))`;
+  }
+  else {
+    return `${geomType}(${nodeString})`;
+  }
+};
+
+// Private Functions!!! Do Not export
+const geometryType = (wkt) => {
+  if (wkt.includes('POLYGON')) {
+    return 'POLYGON';
+  }
+  else if (wkt.includes('LINESTRING')) {
+    return 'LINESTRING';
+  }
+  else if (wkt.includes('POINT')) {
+    return 'POINT';
+  }
+  else {
+    return false;
+  }
 }
 
-const extractNumbers = (numbers) => {
-  const nodes = numbers.split(', ');
-  const nodeObj = nodes
-    .map(n =>
-      n.split(' ')
-    ).map(node => node.map(coord => Number(parseFloat(coord).toFixed(2))));
-  return nodeObj;
+const removeText = (geomType, wkt) => {
+  const stringRemoved = wkt.replace(geomType, '');
+  const nodeString = stringRemoved.replace(/\(/gi, '').replace(')', '');
+  return nodeString;
 }
 
-const toWkt = (nodes) => {
-  const nodeString = nodes.map(xy => `${xy[0]} ${xy[1]}`).join(', ')
-  return `POLYGON((${nodeString}))`
+const extractNumbers = (geomType, nodeString) => {
+  const numbers = nodeString.trim();
+  if (geomType === 'POINT') {
+    const pointNodes = numbers.split(' ').map(coord => {
+      return Number(parseFloat(coord).toFixed(2));
+    });
+    return pointNodes;
+  }
+  const nodes = numbers.split(',');
+  const splitCoords = nodes.map(n => n.trim().split(' '));
+  const numberCoords = splitCoords.map((node) => {
+    return node.map(coord => Number(parseFloat(coord).toFixed(2)));
+  });
+  return numberCoords;
 }
+
+
 module.exports =  {fromWkt, toWkt};
